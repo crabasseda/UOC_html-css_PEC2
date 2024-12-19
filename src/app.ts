e4f109ed35e5ab79586a787d96c660352d7e43b0
[clara rabasseda cases] <[crabasseda@uoc.edu]>
2024-12-19 21:40:24 +0100

    image cleaner

--- src/app.ts
+++ src/app.ts
@@ -8,3 +8,31 @@ document.getElementById("moreInfoBtn")?.addEventListener("click", function () {
     }
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const articles = document.querySelectorAll(".article-list li");
+  const overlay = document.getElementById("image-overlay") as HTMLElement;
+  const overlayImage = document.getElementById(
+    "overlay-image"
+  ) as HTMLImageElement;
+
+  articles.forEach((article) => {
+    article.addEventListener("click", () => {
+      const imageId = article.id.replace("show-", "");
+
+      const image = document.getElementById(imageId) as HTMLImageElement;
+
+      if (image) {
+        overlayImage.src = image.src;
+        overlay.classList.add("active");
+        overlayImage.classList.add("active");
+      }
+    });
+  });
+  overlay.addEventListener("click", (e) => {
+    if (e.target === overlay) {
+      overlay.classList.remove("active");
+      overlayImage.classList.remove("active");
+    }
+  });
+});
