10f19ba5be49ab72cb6a00c5b04c34e01f455f61
[clara rabasseda cases] <[crabasseda@uoc.edu]>
2025-01-13 18:40:09 +0100

    map PSI

--- src/map/map.ts
+++ src/map/map.ts
@@ -1,13 +1,10 @@
 import * as L from "leaflet";
 import "./map.scss";
 
-const app = document.getElementById("map");
-if (app) {
-  const map = L.map("map").setView([26.8206, 30.8025], 6); // Coordenadas de Egipto
+const map = L.map("map").setView([26.8206, 30.8025], 6);
 
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 18,
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
-}
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 18,
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
